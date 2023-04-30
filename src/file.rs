c869f956115e05a18897856b1ba497821fa2091e
zetah11 <findusxi@protonmail.com>
2023-04-30 18:33:27 +0200

    :sparkles: watch

--- src/file.rs
+++ src/file.rs
@@ -1,13 +1,14 @@
+use std::path::PathBuf;
 use std::{fs, io};
 
 use crate::error::Sources;
 
-pub fn get_sources(paths: impl IntoIterator<Item = String>) -> io::Result<Sources> {
+pub fn get_sources(paths: impl IntoIterator<Item = PathBuf>) -> io::Result<Sources> {
     let mut sources = Sources::new();
 
     for path in paths {
         let content = fs::read_to_string(&path)?;
-        sources.add(path, content);
+        sources.add(path.to_string_lossy().into_owned(), content);
     }
 
     Ok(sources)
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,59 @@
 mod error;
 mod file;
 
-use std::io::stderr;
-use std::path::Path;
+use std::io::{stderr, stdin};
+use std::path::{Path, PathBuf};
+use std::time::Duration;
 
 use mm_eval::eval::Evaluator;
-use mm_eval::{compile, Name};
+use mm_eval::Name;
 use mm_media::midi::Pitch;
 use mm_media::{midi, svg};
+use notify_debouncer_mini::notify::RecursiveMode;
+use notify_debouncer_mini::{new_debouncer, DebounceEventResult};
 use typed_arena::Arena;
 
 const MAX_DEPTH: usize = 100;
 const MAX_NOTES: usize = 1000;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let (args, paths) = Args::new(std::env::args().skip(1));
+
+    if args.watch {
+        let mut debouncer = new_debouncer(
+            Duration::from_millis(500),
+            None,
+            move |ev: DebounceEventResult| match ev {
+                Ok(e) => compile(&args, e.into_iter().map(|e| e.path)).unwrap(),
+                Err(e) => println!("watch error {e:?}"),
+            },
+        )?;
+
+        for path in paths {
+            debouncer
+                .watcher()
+                .watch(&path, RecursiveMode::NonRecursive)?;
+        }
+
+        stdin().read_line(&mut String::new())?;
+        Ok(())
+    } else {
+        compile(&args, paths)
+    }
+}
+
+fn compile(
+    args: &Args,
+    paths: impl IntoIterator<Item = PathBuf>,
+) -> Result<(), Box<dyn std::error::Error>> {
     let implicits = Arena::new();
     let explicits = Arena::new();
 
-    let args = Args::new(std::env::args().skip(1));
-
-    let sources = file::get_sources(args.paths)?;
+    let sources = file::get_sources(paths)?;
     let sources = sources.cache();
 
     for (path, source) in sources.iter() {
-        let program = match compile(&implicits, &explicits, source) {
+        let program = match mm_eval::compile(&implicits, &explicits, source) {
             Ok(program) => program,
             Err(es) => {
                 let mut writer = stderr().lock();
@@ -58,10 +88,11 @@ fn write(
     eval: &Evaluator<Pitch>,
 ) -> Result<(), Box<dyn std::error::Error>> {
     let out = path.with_extension(kind.extension());
+    let notes = eval.iter().take(MAX_NOTES);
 
     match kind {
-        Kind::Midi => midi::write(eval.iter().take(MAX_NOTES), &out)?,
-        Kind::Svg => svg::write(eval.iter().take(MAX_NOTES), &out)?,
+        Kind::Midi => midi::write(notes, &out)?,
+        Kind::Svg => svg::write(notes, &out)?,
     }
 
     Ok(())
@@ -70,21 +101,23 @@ fn write(
 struct Args {
     make_midi: bool,
     make_svg: bool,
-    paths: Vec<String>,
+    watch: bool,
 }
 
 impl Args {
-    pub fn new(args: impl IntoIterator<Item = String>) -> Self {
+    pub fn new(args: impl IntoIterator<Item = String>) -> (Self, Vec<PathBuf>) {
         let mut make_midi = false;
         let mut make_svg = false;
+        let mut watch = false;
 
         let mut paths = Vec::new();
 
         for arg in args {
             match arg.as_str() {
-                "--midi" => make_midi = true,
-                "--svg" => make_svg = true,
-                _ => paths.push(arg),
+                "-m" | "--midi" => make_midi = true,
+                "-s" | "--svg" => make_svg = true,
+                "-w" | "--watch" => watch = true,
+                _ => paths.push(PathBuf::from(arg)),
             }
         }
 
@@ -92,11 +125,13 @@ impl Args {
             make_midi = true;
         }
 
-        Args {
+        let args = Args {
             make_midi,
             make_svg,
-            paths,
-        }
+            watch,
+        };
+
+        (args, paths)
     }
 }
 
